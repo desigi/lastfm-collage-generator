0784b3054df67f80277382a70617394a06fa296b
desigi <me@desigi.com>
2020-01-18 00:17:32 -0500

    modified api call

--- script.es5.js
+++ script.es5.js
@@ -128,7 +128,7 @@ function getImageLinks() {
 
   var callApi = function callApi() {
     setUrlFromLimit();
-    axios.get('https://lastfm.desigi.com/repeater.php?url=http:' + collageInfo.url).then(function(_ref) {
+    axios.get(collageInfo.url).then(function(_ref) {
       var data = _ref.data;
 
       console.log(data);
