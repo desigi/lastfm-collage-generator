9733ed16b5dfeef3bf28cd185257f494e2311224
desigi <me@desigi.com>
2020-01-22 14:53:19 -0500

    Changed downloaded file format

--- script.es5.js
+++ script.es5.js
@@ -332,7 +332,7 @@ function registerDownloaded() {
 }
 
 function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL('image/jpeg');
+    link.href = document.getElementById(canvasId).toDataURL('image/png');
     link.download = filename;
 }
 
